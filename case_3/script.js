8772ae705ff82e75cf6f2659e0a0df0145c475e4
Soner Şimşek <sonersimsekhd@gmail.com>
2022-06-13 11:00:01 +0300

    update

--- case_3/script.js
+++ case_3/script.js
@@ -1,25 +1,42 @@
-const disable = document.querySelector('#disableB')
-const enable = document.querySelector('#enableB')
-const tstImg = document.getElementById('demoImg')
+const disable = document.querySelector("#disableB");
+const enable = document.querySelector("#enableB");
+const testImg = document.getElementById("demoImg");
 
-function disableScrolling(){
-    console.log("scroll disable rn")
-    var x = window.scrollX;
-    var y = window.scrollY;
-    window.onscroll = function(){window.scrollTo(x, y);};
+function disableScrolling() {
+  console.log("scroll disable rn");
+  var x = window.scrollX;
+  var y = window.scrollY;
+  window.onscroll = function () {
+    window.scrollTo(x, y);
+  };
 }
 
-function enableScrolling(){
-    console.log("scroll enable rn")
-    window.onscroll = function(){};
+function enableScrolling() {
+  console.log("scroll enable rn");
+  window.onscroll = function () {};
 }
 
-function addFilterBlur(){
-    console.log("img-blur eklendi")
-    tstImg.classList.add("img-blur");
+function addFilterBlur() {
+  console.log("img-blur eklendi");
+  testImg.classList.add("img-blur");
 }
 
-function disableFilterBlur(){
-    console.log("img-blur çıkarıldı")
-    tstImg.classList.remove("img-blur");
+function disableFilterBlur() {
+  console.log("img-blur çıkarıldı");
+  testImg.classList.remove("img-blur");
 }
+
+window.addEventListener("scroll", function () {
+  document.getElementById("showScroll").innerHTML = window.pageYOffset + "px";
+  if (window.pageYOffset >= 300 && window.pageYOffset <= 700) {
+     testImg.classList.add("img-blur");
+    //disableScrolling();
+  } else {
+    testImg.classList.remove("img-blur");
+    //enableScrolling();
+  }
+});
+
+let rect = testImg.getBoundingClientRect();
+console.log("x: "+ rect.x);
+console.log("y: "+ rect.y);
